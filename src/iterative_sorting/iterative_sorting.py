742cb97a35174c138d0e67fe00d568b92dc0f5e7
alexis-anderson <aanderson9313@gmail.com>
2020-09-30 23:12:55 -0400

    mvp done, some stretch

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -7,18 +7,29 @@ def selection_sort(arr):
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
         # Your code here
- #
+        for j in range(i + 1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
 
         # TO-DO: swap
         # Your code here
-
+        arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
-
+    n = len(arr)
+    #traverse through all array elements
+    for i in range(n):
+        swapped = False
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1] : 
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if swapped == False:
+            break
 
     return arr
 
@@ -39,8 +50,34 @@ buckets.
 
 What is the time and space complexity of the counting sort algorithm?
 '''
-def counting_sort(arr, maximum=None):
+def counting_sort(arr, maximum = None):
     # Your code here
 
+    max_element = int(max(arr, default = 0))
+    min_element = int(min(arr, default = 0))
+    range_of_elements = max_element - min_element + 1
+        #create count array to store count of individual
+        #elements and init count array as 0
+    count_arr = [0 for _ in range(range_of_elements)]
+    output_arr = [0 for _ in range(len(arr))]
+    
+    #store count of each character
+    for i in range(0, len(arr)):
+        count_arr[arr[i]-min_element] += 1
+        
+    #change count_arr[i] so that count_arr[i] now contains actual
+    #position of this element in output arr
+    for i in range(1, len(count_arr)):
+        count_arr[i] += count_arr[i-1]
+    
+    #build the output char array
+    for i in range(len(arr)-1, -1, -1):
+        output_arr[count_arr[arr[i] - min_element] - 1] = arr[i]
+        count_arr[arr[i] - min_element] -= 1
+        
+    #copy the output array to arr, so that arr now
+    #contains sorted chars
+    for i in range(0, len(arr)):
+        arr[i] = output_arr[i]
 
     return arr
